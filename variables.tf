4baa07fe2b0d9de7defac14b37c7f392837fc6c1
Ondrej <ondrejskotak@seznam.cz>
2025-07-25 16:15:58 +0200

    files pripravit a main varriables v1

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "region" {
+  description = "AWS region kde se vytvoří instance."
+  type        = string
+  default     = "eu-central-1"
+
+ }
+
+variable "instance_type" {
+  description = "typ instance"
+  type        = string
+  default     = "t3.micro"
+   
+ }
+
+variable "ami_id" {
+  description = "ID AMI pro vytvoření instance."
+  type        = string
+  default     = "ami-08a6efd148b1f7504"
+
+}
+
+variable "prefix" {
+    description = "Prefix"
+    type        = string
+    default     = "terra"
+  }
+
+variable "environment" {
+    description = "Environment"
+    type        = string
+    default     = "test"
+    
+  }
+
+variable "kurz" {
+    description = "Kurz"
+    type        = string
+    default     = "DevOps"
+    
+  }
+
+variable "public_key_path" {
+  description = "Cesta k veřejnému SSH klíči"
+  type        = string
+  default     = "C:/Users/START/.ssh/terra-key.pub"
+}
+
+variable "key_name" {
+  description = "Název SSH klíče"
+  type        = string
+  default     = "terra-key"
+}
+
+variable "instance_count" {
+  description = "Počet EC2 instancí"
+  type        = number
+  default     = 2
+}
